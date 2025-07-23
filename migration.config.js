6c7c8bb49b08e655ae080a596c31b3afb1617df9
RajbharDeepanjli <153265058+Rajbhardeepanjli@users.noreply.github.com>
2025-07-23 23:59:38 +0530

    Delete migration.config.js

--- migration.config.js
+++
@@ -1,9 +0,0 @@
-import dotenv from 'dotenv'
-dotenv.config();
-
-module.exports={
-    migrationTable: 'pgmigrations',
-    dir: 'migrations',
-    direction: 'up',
-    databaseUrl: process.env.DATABASE_URL
-}
